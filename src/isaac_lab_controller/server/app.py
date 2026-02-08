cdea7971022c0cbe0936475692f05352323be9c5
Jongwon <infinity21y@gmail.com>
2026-02-08 21:43:14 +0900

    Modified run error
    Signed-off-by: Jongwon <infinity21y@gmail.com>

--- src/isaac_lab_controller/server/app.py
+++ src/isaac_lab_controller/server/app.py
@@ -16,7 +16,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 
 from isaac_lab_controller.adapters.base import SceneAdapter
-from isaac_lab_controller.server.routes import camera, objects, materials, rl, robot, data_collection
+from isaac_lab_controller.server.routes import camera, objects, materials, robot, data_collection
 from isaac_lab_controller.server.websocket_handler import WebSocketHandler
 from isaac_lab_controller.utils.config import load_config, ControllerConfig
 
--- src/isaac_lab_controller/server/routes/__init__.py
+++ src/isaac_lab_controller/server/routes/__init__.py
@@ -2,6 +2,6 @@
 라우트 모듈
 """
 
-from isaac_lab_controller.server.routes import camera, objects, materials, rl, robot, data_collection
+from isaac_lab_controller.server.routes import camera, objects, materials, robot, data_collection
 
-__all__ = ["camera", "objects", "materials", "rl", "robot", "data_collection"]
+__all__ = ["camera", "objects", "materials", "robot", "data_collection"]
